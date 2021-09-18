5b1f3252e822502694b1a46dde32a76b3fb8fb51
blockbuidl <terrys999@gmail.com>
2021-09-18 17:01:17 +0000

    ddd

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ var ingredients = [
 ];
 
 
-app.get('/ingredients', function(req, res) {
+app.get('/api', function(req, res) {
     console.log("GET From SERVER");
     res.send(ingredients);
 });
